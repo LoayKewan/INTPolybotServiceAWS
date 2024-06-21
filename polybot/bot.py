e8c18d6aae46da5e719b2675a0aefc75db0868e8
loaykewan <loaykewan4@gmail.com>
2024-06-21 16:13:55 +0300

    newcomment1

--- polybot/bot.py
+++ polybot/bot.py
@@ -9,8 +9,8 @@ class Bot:
         self.telegram_bot_client = telebot.TeleBot(token)
         self.telegram_bot_client.remove_webhook()
         time.sleep(0.5)
-
-        retries = 4 
+        #####################################################
+        retries = 4
         for _ in range(retries):
             try:
                 #self.telegram_bot_client.set_webhook(url=f'{telegram_chat_url}/{token}/', timeout=60)
@@ -31,49 +31,55 @@ class Bot:
 
     def send_text(self, chat_id, text):
         self.telegram_bot_client.send_message(chat_id, text)
-        time.sleep(3)
 
     def send_text_with_quote(self, chat_id, text, quoted_msg_id):
         self.telegram_bot_client.send_message(chat_id, text, reply_to_message_id=quoted_msg_id)
-        time.sleep(3)
 
     def is_current_msg_photo(self, msg):
         return 'photo' in msg
 
     def download_user_photo(self, msg):
+        """
+        Downloads the photos that sent to the Bot to `photos` directory (should be existed)
+        :return:
+        """
         if not self.is_current_msg_photo(msg):
             raise RuntimeError(f'Message content of type \'photo\' expected')
+
         file_info = self.telegram_bot_client.get_file(msg['photo'][-1]['file_id'])
         data = self.telegram_bot_client.download_file(file_info.file_path)
         folder_name = file_info.file_path.split('/')[0]
+
         if not os.path.exists(folder_name):
             os.makedirs(folder_name)
+
         with open(file_info.file_path, 'wb') as photo:
             photo.write(data)
-        time.sleep(3)
+
         return file_info.file_path
 
     def send_photo(self, chat_id, img_path):
         if not os.path.exists(img_path):
             raise RuntimeError("Image path doesn't exist")
+
         self.telegram_bot_client.send_photo(
             chat_id,
             InputFile(img_path)
         )
-        time.sleep(3)
 
     def handle_message(self, msg):
+        """Bot Main message handler"""
         logger.info(f'Incoming message: {msg}')
         self.send_text(msg['chat']['id'], f'Your original message: {msg["text"]}')
-        time.sleep(3)
+
 
 class ObjectDetectionBot(Bot):
     def handle_message(self, msg):
         logger.info(f'Incoming message: {msg}')
-        if "text" in msg and msg["text"] == "hi":
-            self.send_text(msg['chat']['id'], f"Hi: {msg['chat']['first_name']} {msg['chat']['last_name']}, how can I help you?")
+
         if self.is_current_msg_photo(msg):
             photo_path = self.download_user_photo(msg)
+
             # TODO upload the photo to S3
             # TODO send a job to the SQS queue
             # TODO send message to the Telegram end-user (e.g. Your image is being processed. Please wait...)
